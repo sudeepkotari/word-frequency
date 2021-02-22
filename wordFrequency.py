a3edd92fe78524d74ed52296ddf067776aa9e0de
sudeepkotari <sudeepkotari@gmail.com>
2021-02-22 21:04:23 +0530

    program optimized

--- wordFrequency.py
+++ wordFrequency.py
@@ -5,9 +5,9 @@ try:
    for line in file:
        words = line.split()
        for word in words:
-        #d1[word] = d1.get(word,0) + 1 
+        #d1[word] = d1[word] + 1  
         if word in d1:
-            d1[word] = d1[word] + 1  
+            d1[word] = d1.get(word,0) + 1 
         else:
             d1[word] =1
    print(d1)
