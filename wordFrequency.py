57fd99e8b2db4ed31054f3a5839e4308e1067098
sudeepkotari <sudeepkotari@gmail.com>
2021-02-22 20:39:37 +0530

    initial commit

---
+++ wordFrequency.py
@@ -0,0 +1,16 @@
+fname = input("enter the filename: ")
+try:
+   file = open(fname)
+   d1 = dict()
+   for line in file:
+       words = line.split()
+       for word in words:
+        #d1[word] = d1.get(word,0) + 1 
+        if word in d1:
+            d1[word] = d1[word] + 1  
+        else:
+            d1[word] =1
+   print(d1)
+    
+except:
+     print("file not found to display")
